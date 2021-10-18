674f97e67ca4c6c3d120beb4ac251a0535eeacf0
FilipPtak <91986584+FilipPtak@users.noreply.github.com>
2021-10-19 00:32:23 +0200

    Delete 1st Semester/02 - Types and Variables directory

--- 1st Semester/02 - Types and Variables/display the Results of Three Dice Rolls and the Sum of the Dice Rolled.py
+++
@@ -1,9 +0,0 @@
-import random
-
-one = random.randrange(1,6)
-two = random.randrange(1,6)
-three = random.randrange(1,6)
-print('diceroll one: {}\ndiceroll two: {}\ndiceroll three: {}'.format(one, two, three))
-
-sum = one + two + three
-print('sum of the dice rolled: ', sum)
