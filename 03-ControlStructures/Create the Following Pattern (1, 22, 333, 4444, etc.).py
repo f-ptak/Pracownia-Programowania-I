1dd9586fb30e769393f8b03f79df7ff96dc7152c
Filip <91986584+PtakFilip@users.noreply.github.com>
2021-10-23 20:44:52 +0200

    Add files via upload

---
+++ 03-ControlStructures/Create the Following Pattern (1, 22, 333, 4444, etc.).py
@@ -0,0 +1,3 @@
+for x in range (1,10):
+    y = str(x)
+    print(y*x)
---
+++ 03-ControlStructures/create the Following Rectangle with Dimensions A and B.py
@@ -0,0 +1,11 @@
+a = int(input('Enter dimension a: '))
+b = int(input('Enter dimension b: '))
+
+print('*'*b)
+
+i=0
+while i<(a-2):
+    print('*',' '*(b-4),'*')
+    i += 1
+
+print('*'*b)
