9816c9f31512d15752289a63762ed5fc54c12e58
Filip <91986584+PtakFilip@users.noreply.github.com>
2021-10-23 20:45:53 +0200

    Add files via upload

---
+++ 03 - Control Structures/create a Multiplication Table in the Range 1 to 10 For Any Number Entered by the User.py
@@ -0,0 +1,7 @@
+number = int(input('Enter a number from 1 to 10: '))
+
+i=1
+while i < 11:
+    result = number * i
+    print(f'{number} x {i} = {result}')
+    i += 1
---
+++ 03 - Control Structures/display the Contents of the Variable With an Extra Space Between Characters.py
@@ -0,0 +1,5 @@
+sentence = input('Enter a sentence: ')
+
+print(sentence)
+# print(' '.join(sentence))
+print(sentence.replace('', ' ')[1: -1])
