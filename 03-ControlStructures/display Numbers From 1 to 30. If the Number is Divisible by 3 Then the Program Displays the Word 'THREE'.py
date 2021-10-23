b81dc28a177a1a3613826bb801b11e45e37c931f
Filip <91986584+PtakFilip@users.noreply.github.com>
2021-10-23 20:45:04 +0200

    Delete 03-ControlStructures directory

--- 03-ControlStructures/display Numbers From 1 to 30. If the Number is Divisible by 3 Then the Program Displays the Word 'THREE'.py
+++
@@ -1,9 +0,0 @@
-for x in range(1,31):
-    if (x % 3 == 0) and (x % 5 == 0):
-        print ('BINGO')
-    elif (x % 5 == 0):
-        print('FIVE')
-    elif (x % 3 == 0):
-        print('THREE')
-    else:
-        print(x)
--- 03-ControlStructures/display the Contents of the Variable With an Extra Space Between Characters.py
+++
@@ -1,5 +0,0 @@
-sentence = input('Enter a sentence: ')
-
-print(sentence)
-# print(' '.join(sentence))
-print(sentence.replace('', ' ')[1: -1])
