272b7aa23ba9d5d237ca9dabdeea7761839e99c0
PtakFilip <s222693@student.uek.krakow.pl>
2022-01-03 23:07:45 +0100

    11.11

--- 11 Inheritance/11.11 create a class describing a student. Create a class variable to store the last student’s ID number.py
+++ 11 Inheritance/11.11 create a class describing a student. Create a class variable to store the last student’s ID number.py
@@ -6,4 +6,15 @@ class Student():
         self.surname = surname
         self.studyfield = studyfield
     
-    
+    def __str__(self):
+        return (self.name).capitalize() + " " + (self.surname).upper() + " (" + (str(self.studentID)) + "), " + self.studyfield
+
+student1 = Student("Ludwig", "van Beethoven","Filozofia, UEK Kraków")
+Student.studentID += 1
+print(student1)
+student2 = Student("Giuseppe", "Verdi","Kulturoznastwo, UEK Kraków")
+Student.studentID += 1
+print(student2)
+student3 = Student("Frédéric", "Chopin","Socjologia, UEK Kraków")
+Student.studentID += 1
+print(student3)
