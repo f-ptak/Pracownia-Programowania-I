cf3f82c18055da79c92b75916a5f517859a8d54f
PtakFilip <s222693@student.uek.krakow.pl>
2022-01-05 12:55:53 +0100

    F5

--- 12.1 Mock Test 3/F5.py
+++ 12.1 Mock Test 3/F5.py
@@ -3,10 +3,28 @@ class C():
         self.t = t
     
     def m1(self):
-        pass
+        fulldic = {}
+        
+        for i in range(len(self.t)):
+            if self.t[i] not in fulldic:
+                fulldic[self.t[i]] = 1
+            else:
+                fulldic[self.t[i]] += 1
+                
+        return fulldic
     
     def m2(self,let):
-        pass
+        seldic = {}
+        
+        for i in range(len(let)):
+            if let[i] not in seldic:
+                seldic[let[i]] = 0
+        
+        for i in range(len(self.t)):
+            if self.t[i] in seldic:
+                seldic[self.t[i]] += 1
+        
+        return seldic
 
 
 print(C("my moon").m1())
