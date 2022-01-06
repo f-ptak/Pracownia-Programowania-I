082987498d4ac82129fc3dfcf10abebf1db48f25
PtakFilip <s222693@student.uek.krakow.pl>
2022-01-06 14:08:47 +0100

    F8

--- 12.1 Mock Test 3/F8.py
+++ 12.1 Mock Test 3/F8.py
@@ -1,5 +1,5 @@
 class C():
-# obydwie metody robią to samo, tylko w inny sposób
+# wszystkie metody robią to samo, tylko w inny sposób
 # ta metoda za każdym razem przegląda każdą pozycję w poszukiwaniu tagu "wife" i "children"
     def m(self, n1, n2):
         import xml.etree.ElementTree as ET
@@ -47,8 +47,26 @@ class C():
                 counter += 1
                 
         return counter
+    
+# ta metoda powinna być najbardziej zoptymalizowana i wykorzystywać funkcje findall() i find(), zamiast loop'ować jak w pozostałych funkcjach
+    def m3(self, n1, n2):
+        import xml.etree.ElementTree as ET
+        tree = ET.parse("mockdata.xml")
+        root = tree.getroot()
+        
+        counter = 0
+        for family in root.findall("family"):
+            
+            wife = family.find("wife")
+            children = family.find("children")
+            
+            if int(wife.find("age").text) >= n1 and len(children) >= n2:
+                counter += 1
+        
+        return counter
 
-age = 40
-children = 2
-print(C().m(age, children))
-print(C().m2(age, children))
+wifeage = 40
+childrenamount = 2
+print(C().m(wifeage, childrenamount))
+print(C().m2(wifeage, childrenamount))
+print(C().m3(wifeage, childrenamount))
